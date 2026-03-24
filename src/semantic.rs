a12b08cc9436fddcf22fb56f7e466b21c4a66f79
chadsec1 <freakah1@gmail.com>
2026-03-24 12:45:07 +0200

    refactor: Code cleanup

--- src/semantic.rs
+++ src/semantic.rs
@@ -10,6 +10,7 @@ use crate::parser::{
 #[cfg(test)]
 mod tests;
 
+mod branch_analysis;
 mod helpers;
  
 
@@ -31,8 +32,8 @@ struct VarInfo {
                        // but it's something to keep in mind.
 }
 
-/// Public entry: check semantics and fill in inferred types where possible.
-/// This mutates the AST: Type::Infer are replaced with concrete types where
+/// Checks semantics and fill in inferred types where possible.
+/// This mutates the AST, Type::Infer are replaced with concrete types where
 /// they can be inferred.
 pub fn check_semantics(ast: &mut AST) -> Result<(), HolyError> {
     // First pass: collect function signatures (params types and return types (possible multiple))
@@ -52,7 +53,7 @@ pub fn check_semantics(ast: &mut AST) -> Result<(), HolyError> {
     Ok(())
 }
 
-/// Check single function: infer local var types where possible, check calls, returns.
+/// Check single function, infer local var types where possible, check calls, returns.
 fn check_function(func: &mut Function, fun_sigs: &HashMap<String, (Vec<Type>, Option<Vec<Type>>)>) -> Result<(), HolyError> {
     // Build local symbol table starting with params
     let mut locals: HashMap<String, VarInfo> = HashMap::new();
@@ -108,7 +109,7 @@ fn check_function(func: &mut Function, fun_sigs: &HashMap<String, (Vec<Type>, Op
 
 
     if let Some(ret_ty) = &func.return_type {
-        return_branch_analysis(&func.clone(), last_func_stmt.cloned(), func.span, false, false)?;
+        branch_analysis::return_branch_analysis(&func.clone(), last_func_stmt.cloned(), func.span, false, false)?;
     }
 
     Ok(())
@@ -116,165 +117,6 @@ fn check_function(func: &mut Function, fun_sigs: &HashMap<String, (Vec<Type>, Op
 }
 
 
-
-fn return_branch_analysis(
-    func: &Function,
-    last_stmt: Option<Stmt>,
-    span: Span,
-    is_loop: bool,
-    forbid_break: bool
-) -> Result<(), HolyError> {
-    let ret_ty = func.return_type.clone().unwrap();
-
-    match last_stmt {
-
-        Some(Stmt::Break(breakStmt)) => {
-            if forbid_break {
-                return Err(HolyError::Semantic(format!(
-                        "You cannot `break` out of a forever loop if its the last statement in a function that returns. Use a return statement instead. (line {} column {})",
-                        breakStmt.span.line, breakStmt.span.column,
-                    )));
-            }
-
-        }
-
-        Some(Stmt::Return(_)) => {}
-
-        Some(Stmt::Forever(foreverStmt)) => {
-
-            // If we are in a loop, we dont care about breaks or whatever.
-            if !is_loop {
-                // So, why do we error on break? can't programmer like break then return outside for
-                // loop?
-                // Answer is that return_branch_analysis is only called on last statemet, and if
-                // forever loop is last statement, you can't break out of it. You can only return, or
-                // you dont return but you don't break.
-                //
-                for s in &foreverStmt.branch {
-                    match s {
-                        Stmt::Break(breakStmt) => {
-
-                            // If this is a nested loop, like a forever inside another forever, you can
-                            // break out of it fine.
-                            if !is_loop {
-                                return Err(HolyError::Semantic(format!(
-                                    "You cannot `break` out of a forever loop if its the last statement in a function that returns. Use a return statement instead. (line {} column {})",
-                                    breakStmt.span.line, breakStmt.span.column,
-                                )));
-                                
-                            }
-                        }
-
-                        Stmt::If(stmt) => {
-                            return_branch_analysis(func, Some(s.clone()), stmt.span, true, true)?;
-                        }
-
-
-                        Stmt::While(stmt) => {
-                            return_branch_analysis(func, Some(s.clone()), stmt.span, true, false)?;
-                        }
-
-                        Stmt::Forever(stmt) => {
-                            return_branch_analysis(func, Some(s.clone()), stmt.span, true, false)?;
-                        }
-
-
-
-                        // Skip all other statements
-                        _ => {}
-                    }
-                }
-            }
-        }
-
-        Some(Stmt::While(whileStmt)) => {
-            // If this is a nested loop, like a while loop inside a `forever` loop, we let you do
-            // that. if in_loop is true, it might not be last statement after all.
-            //
-            if !is_loop {
-                return Err(HolyError::Semantic(format!(
-                        "While loops may or may not execute at all, therefore you need a return statement outside the loop scope, or consider using `forever` infinite loops instead. (line {} column {})",
-                        whileStmt.span.line, whileStmt.span.column,
-                    )));
-            
-            }
-        }
-        
-        Some(Stmt::For(forStmt)) => return Err(HolyError::Semantic(format!(
-                    "For loops may or may not execute at all, therefore you need a return statement outside the loop scope. (line {} column {})",
-                    forStmt.span.line, forStmt.span.column,
-                ))),
-        
-
-        Some(Stmt::If(ifStmt)) => {
-
-            // If we are not in a loop, then we only care about last statement of if branches
-            // bodies
-            if !is_loop {
-                let stmt = ifStmt.if_branch.last();
-                return_branch_analysis(func, stmt.cloned(), ifStmt.span, is_loop, forbid_break)?;
-
-                if ifStmt.else_branch.is_none() {
-                    return Err(HolyError::Semantic(format!(
-                        "Function `{}` only returns in if statement branches, which might not always execute. Add an `else` branch (line {} column {})",
-                        func.name, ifStmt.span.line, ifStmt.span.column,
-                    )));
-                }
-
-                let stmt = ifStmt.else_branch.as_ref().unwrap().last();
-
-                return_branch_analysis(func, stmt.cloned(), ifStmt.span, is_loop, forbid_break)?;
-
-
-                for s_vec in &ifStmt.elif_branches {
-                    let body = &s_vec.1;
-
-                    let stmt = body.last();
-                    return_branch_analysis(func, stmt.cloned(), ifStmt.span, is_loop, forbid_break)?;
-                }
-
-            } else {
-                for stmt in &ifStmt.if_branch {
-                    return_branch_analysis(func, Some(stmt).cloned(), ifStmt.span, is_loop, forbid_break)?;
-                }
-                
-                // We dont care if else branch is none, we in a loop. 
-                if ifStmt.else_branch.is_some() {
-                    for stmt in &ifStmt.else_branch.unwrap() {
-                        return_branch_analysis(func, Some(stmt).cloned(), ifStmt.span, is_loop, forbid_break)?;
-                    }
-                }
-
-                for s_vec in &ifStmt.elif_branches {
-                    let body = &s_vec.1;
-
-
-                    for stmt in body {
-                        return_branch_analysis(func, Some(stmt).cloned(), ifStmt.span, is_loop, forbid_break)?;
-                    }
-                }
-
-
-            }
-        },
-        Some(other) => {
-            if !is_loop {
-                let branch_span = helpers::stmt_span(&other);
-
-                return Err(HolyError::Semantic(format!(
-                    "Function `{}` declares return type(s) `{:?}`, but statement branch body does not end with a return statement (line {} column {})",
-                    func.name, ret_ty, branch_span.line, branch_span.column,
-                ))) 
-            }
-        },
-
-
-        _ => panic!("(Compiler bug) check_stmts should've errored when it encounterd an empty block, but it didn't:\nFunc: {:?}\nlast_stmt: {:?}", func, last_stmt)
-    }
-
-
-    Ok(())
-}
 
 
 // Parse stmts in a block
