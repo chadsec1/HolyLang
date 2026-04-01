549d3c6e4776ba804153753ff5716c9f1b001d75
chadsec1 <freakah1@gmail.com>
2026-04-01 05:29:29 +0200

    fix: Add range as a reserved language keyword

--- src/consts.rs
+++ src/consts.rs
@@ -3,7 +3,7 @@
 /// No variables, or function names, can use them.
 /// case-insensitive.
 pub const RESERVED_KEYWORDS: &[&str] = &[
-    "func", "own", "infinite", "return", "for", "in", "if", "elif", "else", "true", "false",
+    "func", "own", "infinite", "return", "for", "in", "range", "if", "elif", "else", "true", "false",
     "int8", "int16", "int32", "int64", "int128", "byte", "uint16", "uint32", "uint64",
     "uint128", "float32", "float64", "usize", "bool", "string", "copy", "format",
     "lock", "unlock", "while", "break", "continue"
