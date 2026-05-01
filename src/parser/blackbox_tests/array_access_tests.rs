ce991ef989cfdd98c82df0ef9d9f3a965e92c829
ChadSec <freakah1@gmail.com>
2026-05-01 22:03:47 +0300

    tests: Improve existing tests, and add more tests

--- src/parser/blackbox_tests/array_access_tests.rs
+++ src/parser/blackbox_tests/array_access_tests.rs
@@ -1,51 +1,148 @@
 use super::*;
 
 #[cfg(test)]
-mod array_access_tests {
+mod array_access_tests_in_functions {
     use super::*;
 
     #[test]
     fn array_single_access() {
         let literals_edge_cases = get_all_literals_edge_cases();
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+         
+        for l in letters {
+            let arr_name = format!("{}{}", l, l);
 
-        for t in ALL_TYPES_NO_ARR {
-            for l in &literals_edge_cases {
-                let stmts = parse_body(&format!("own v {} = arr[{}]", t, l));
-                if let Stmt::VarDecl(v) = &stmts[0] {
-                    assert_eq!(v.type_name, t.clone());
-                    assert!(matches!(v.value, Some(Expr::ArrayAccess { .. })));
-                } else { panic!("Expected VarDecl"); }
+            for lit in &literals_edge_cases {
+                let stmts = parse_body(&format!("{}[{}]", arr_name, lit));
+                if let Stmt::Expr(e) = &stmts[0] {
+                    if let Expr::ArrayAccess{ array, .. } = &e {
+                        if let Expr::Var{ name, .. } = &**array {
+                            assert_eq!(name, &arr_name.to_string());
+                        } else { panic!("Expected Var in ArrayAccess expression 'array' field, instead got {:?}", array); }
+
+                    } else { panic!("Expected ArrayAccess expression, instead got {:?}", e); }
+                } else { panic!("Expected Expr, instead got {:?}", stmts); }
             }
         }
     }
 
     #[test]
-    fn array_single_access_no_type_errors() {
-        assert_parse_err(&wrap("own v = arr[0]"));
-    }
-
-
-    #[test]
-    fn array_access_variable_index() {
+    fn array_single_access_in_var_decl() {
         let literals_edge_cases = get_all_literals_edge_cases();
-        
-        for t in ALL_TYPES_NO_ARR {
-            for l in &literals_edge_cases {
-                let stmts = parse_body(&format!("own v {} = arr[{}]", t, l));
-                if let Stmt::VarDecl(v) = &stmts[0] {
-                    assert_eq!(v.type_name, t.clone());
-                    assert!(matches!(v.value, Some(Expr::ArrayAccess { .. })));
-                } else { panic!("Expected VarDecl"); }
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+         
+        for l in letters {
+            let arr_name = format!("{}{}", l, l);
+            for lit in &literals_edge_cases {
+                for t in ALL_TYPES_NO_ARR {
+                    let stmts = parse_body(&format!("own {} {} = {}[{}]", l, t, arr_name, lit));
+                    if let Stmt::VarDecl(v) = &stmts[0] {
+                        assert_eq!(v.name, l.to_string());
+                        assert_eq!(v.type_name, t.clone());
+                        assert!(v.value.is_some());
+                        if let Expr::ArrayAccess{ array, .. } = v.value.clone().unwrap() {
+                            if let Expr::Var{ name, .. } = &*array {
+                                assert_eq!(name, &arr_name.to_string());
+                            } else { panic!("Expected Var in ArrayAccess expression 'array' field, instead got {:?}", array); }
+
+                        } else { panic!("Expected ArrayAccess expression, instead got {:?}", v.value.clone().unwrap()); }
+                        assert!(matches!(v.value, Some(Expr::ArrayAccess { .. })));
+                    } else { panic!("Expected VarDecl"); }
+                }
             }
         }
     }
 
+    #[test]
+    fn array_single_access_in_var_decl_with_no_type_errors() {
+        let literals_edge_cases = get_all_literals_edge_cases();
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+         
+        for l in letters {
+            let arr_name = format!("{}{}", l, l);
+            for lit in &literals_edge_cases {
+                assert_parse_err(&wrap(&format!("own {} = {}[{}]", l, arr_name, lit)));
+            }
+        }
+    }
+}
+
+
+
+
+// Same tests, but now in globals.
+#[cfg(test)]
+mod array_access_tests_in_globals {
+    use super::*;
 
     #[test]
-    fn array_access_variable_index_no_type_errors() {
-        assert_parse_err(&wrap("own v = arr[i]"));
+    fn array_single_access() {
+        let literals_edge_cases = get_all_literals_edge_cases();
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+         
+        for l in letters {
+            let arr_name = format!("{}{}", l, l);
+
+            for lit in &literals_edge_cases {
+                let ast = parse(&format!("{}[{}]", arr_name, lit)).unwrap();
+                assert_eq!(ast.functions.len(), 0);
+                assert_eq!(ast.globals.len(), 1);
+
+                if let Stmt::Expr(e) = &ast.globals[0] {
+                    if let Expr::ArrayAccess{ array, .. } = &e {
+                        if let Expr::Var{ name, .. } = &**array {
+                            assert_eq!(name, &arr_name.to_string());
+                        } else { panic!("Expected Var in ArrayAccess expression 'array' field, instead got {:?}", array); }
+
+                    } else { panic!("Expected ArrayAccess expression, instead got {:?}", e); }
+                } else { panic!("Expected Expr, instead got {:?}", ast); }
+            }
+        }
     }
 
+    #[test]
+    fn array_single_access_in_var_decl() {
+        let literals_edge_cases = get_all_literals_edge_cases();
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+         
+        for l in letters {
+            let arr_name = format!("{}{}", l, l);
+            for lit in &literals_edge_cases {
+                for t in ALL_TYPES_NO_ARR {
+                    let ast = parse(&format!("own {} {} = {}[{}]", l, t, arr_name, lit)).unwrap();
+                    assert_eq!(ast.functions.len(), 0);
+                    assert_eq!(ast.globals.len(), 1);
 
+                    if let Stmt::VarDecl(v) = &ast.globals[0] {
+                        assert_eq!(v.name, l.to_string());
+                        assert_eq!(v.type_name, t.clone());
+                        assert!(v.value.is_some());
+                        if let Expr::ArrayAccess{ array, .. } = v.value.clone().unwrap() {
+                            if let Expr::Var{ name, .. } = &*array {
+                                assert_eq!(name, &arr_name.to_string());
+                            } else { panic!("Expected Var in ArrayAccess expression 'array' field, instead got {:?}", array); }
+
+                        } else { panic!("Expected ArrayAccess expression, instead got {:?}", v.value.clone().unwrap()); }
+                    } else { panic!("Expected VarDecl"); }
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn array_single_access_in_var_decl_with_no_type_errors() {
+        let literals_edge_cases = get_all_literals_edge_cases();
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+         
+        for l in letters {
+            let arr_name = format!("{}{}", l, l);
+            for lit in &literals_edge_cases {
+                assert_parse_err(&format!("own {} = {}[{}]", l, arr_name, lit));
+            }
+        }
+    }
 
 }
+
+
+
--- src/parser/blackbox_tests/var_assign_tests.rs
+++ src/parser/blackbox_tests/var_assign_tests.rs
@@ -3,23 +3,22 @@ use super::*;
 #[cfg(test)]
 mod var_assign_tests {
     use super::*; 
- 
+
+
     #[test]
     fn var_assign() {
         let literals_edge_cases = get_all_literals_edge_cases(); 
-        for t in ALL_TYPES_NO_ARR {
-            for l in &literals_edge_cases {
-                let stmts = parse_body(&format!("own x {}\nx = {}", t, l));
-                assert_eq!(stmts.len(), 2);
-
-                if let Stmt::VarDecl(v) = &stmts[0] {
-                    assert_eq!(v.name, "x");
-                    assert_eq!(v.type_name, t.clone());
-                } else { panic!("Expected VarDecl"); }    
-
-
-                if let Stmt::VarAssign(va) = &stmts[1] {
-                    assert_eq!(va.name, "x");
+        let letters: Vec<char> = ('a'..='z')
+            .chain('A'..='Z')
+            .collect();
+        
+         
+        for l in letters {
+            for lit in &literals_edge_cases {
+                let stmts = parse_body(&format!("{} = {}", l, lit));
+                assert_eq!(stmts.len(), 1);
+                if let Stmt::VarAssign(va) = &stmts[0] {
+                    assert_eq!(va.name, l.to_string());
                 } else {
                     panic!("Expected VarAssign");
                 }
@@ -29,12 +28,87 @@ mod var_assign_tests {
 
 
     #[test]
-    fn var_assign_multi() {
-        let stmts = parse_body("x, y = swap()");
-        if let Stmt::VarAssignMulti(ma) = &stmts[0] {
-            assert_eq!(ma.names, vec!["x", "y"]);
-        } else {
-            panic!("Expected VarAssignMulti");
+    fn var_assign_with_var_decl() {
+        let literals_edge_cases = get_all_literals_edge_cases(); 
+        let letters: Vec<char> = ('a'..='z')
+            .chain('A'..='Z')
+            .collect();
+        
+         
+        for l in letters {
+            for t in ALL_TYPES_NO_ARR {
+                for lit in &literals_edge_cases {
+                    let stmts = parse_body(&format!("own {} {}\n{} = {}", l, t, l, lit));
+                    assert_eq!(stmts.len(), 2);
+
+                    if let Stmt::VarDecl(v) = &stmts[0] {
+                        assert_eq!(v.name, l.to_string());
+                        assert_eq!(v.type_name, t.clone());
+                        assert!(v.value.is_none());
+                    } else { panic!("Expected VarDecl"); }    
+
+
+                    if let Stmt::VarAssign(va) = &stmts[1] {
+                        assert_eq!(va.name, l.to_string());
+                    } else {
+                        panic!("Expected VarAssign");
+                    }
+                }
+            }
         }
     }
+
+    #[test]
+    fn var_re_assign_with_var_decl() {
+        let literals_edge_cases = get_all_literals_edge_cases(); 
+        let letters: Vec<char> = ('a'..='z')
+            .chain('A'..='Z')
+            .collect();
+        
+        for l in letters {
+            for t in ALL_TYPES_NO_ARR {
+                for lit in &literals_edge_cases {
+                    let stmts = parse_body(&format!("own {} {} = {}\n{} = {}", l, t, lit, l, lit));
+                    assert_eq!(stmts.len(), 2);
+
+                    if let Stmt::VarDecl(v) = &stmts[0] {
+                        assert_eq!(v.name, l.to_string());
+                        assert_eq!(v.type_name, t.clone());
+                        assert!(v.value.is_some());
+                    } else { panic!("Expected VarDecl"); }    
+
+
+                    if let Stmt::VarAssign(va) = &stmts[1] {
+                        assert_eq!(va.name, l.to_string());
+                    } else {
+                        panic!("Expected VarAssign");
+                    }
+                }
+            }
+        }
+    }
+
+
+    #[test]
+    fn var_assign_multi() {
+        let letters_1: Vec<char> = ('a'..='z')
+            .chain('A'..='Z')
+            .collect();
+
+        let letters_2: Vec<char> = ('A'..='Z')
+            .chain('a'..='z')
+            .collect();
+        
+        for l1 in letters_1 {
+            for l2 in &letters_2 {
+                let stmts = parse_body(&format!("{}, {} = swap()", l1, l2));
+                if let Stmt::VarAssignMulti(ma) = &stmts[0] {
+                    assert_eq!(ma.names, vec![l1.to_string(), l2.to_string()]);
+                } else {
+                    panic!("Expected VarAssignMulti");
+                }
+            }
+        }
+    }
+
 }
