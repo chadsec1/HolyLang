a023ea5b3e946e8b82f5b12ebd363b38e85f3952
ChadSec <freakah1@gmail.com>
2026-05-05 06:00:48 +0300

    tests: Improve parser blackbox unit tests

--- src/parser/blackbox_tests/bin_op_tests.rs
+++ src/parser/blackbox_tests/bin_op_tests.rs
@@ -177,8 +177,84 @@ mod bin_op_tests_in_functions {
 
 
 
+
     #[test]
     fn binop_arth_unsigned_literals_only() {
+        let edge_cases_numbers = [
+            u8::MIN as u128, u8::MAX as u128, 
+            u16::MIN as u128, u16::MAX as u128, 
+            u32::MIN as u128, u32::MAX as u128, 
+            u64::MIN as u128, u64::MAX as u128, 
+            u128::MIN, u128::MAX, 
+
+            usize::MIN as u128, usize::MAX as u128
+        ];
+
+
+        // Because we default to signed integers literals, unless we go out of range, then we switch to
+        // unsigned literals types.
+        // so those expected types are correct.
+        // Int8 because unsigned::MIN is always 0, which can fit into int8
+        //
+        // I hope this test is not too much voodo for the reader, but it is what it is. It's good,
+        // it's correct, it works, and it catches most parser edge cases.
+        let edge_cases_types = [
+            Type::Int8, Type::Int16,
+            Type::Int8, Type::Int32,
+            Type::Int8, Type::Int64,
+            Type::Int8, Type::Int128,
+            Type::Int8, Type::Uint128,
+            
+            Type::Int8, Type::Int128,
+        ];
+
+        for (en1, et1) in edge_cases_numbers.iter().zip(edge_cases_types.iter()) {    
+            for (en2, et2) in edge_cases_numbers.iter().zip(edge_cases_types.iter()) {    
+                for (b, s) in ALL_BIN_OP_KIND_ARTH.iter().zip(BIN_OP_KIND_ARTH_SYMBOLS.iter()) {
+                    let stmts = parse_body(&format!("{} {} {}", en1, s, en2));
+                    if let Stmt::Expr(e) = &stmts[0] {
+                        if let Expr::BinOp { left, right, op, .. } = &e {
+                            assert_eq!(op, b);
+
+                            if let Expr::IntLiteral { value, .. } = **left {
+                                assert_eq!(value.get_type(), et1.clone());
+
+                                if value.is_signed() {
+                                    assert!(value.as_i128() >= 0);
+                                    assert_eq!(value.as_i128() as u128, *en1);
+                                } else {
+                                    assert_eq!(value.as_u128(), *en1);
+                                } 
+
+                            } else { panic!("Expected IntLiteral, instead got: {:?}", **left) }
+
+
+                            if let Expr::IntLiteral { value, .. } = **right {
+                                assert_eq!(value.get_type(), et2.clone());
+
+
+                                if value.is_signed() {
+                                    assert!(value.as_i128() >= 0);
+                                    assert_eq!(value.as_i128() as u128, *en2);
+                                } else {
+                                    assert_eq!(value.as_u128(), *en2);
+                                } 
+
+                            } else { panic!("Expected IntLiteral, instead got: {:?}", **right) }
+
+
+                        } else {
+                            panic!("Expected {:?}, instead we got {:?}", b, &e);
+                        }
+                    } else { panic!("Expected Expr, instead we got {:?}", &stmts[0]) }
+                }
+            }
+        }
+    }
+
+
+    #[test]
+    fn binop_arth_unsigned_literals_only_in_var_decl() {
         let edge_cases_numbers = [
             u8::MIN as u128, u8::MAX as u128, 
             u16::MIN as u128, u16::MAX as u128, 
@@ -253,11 +329,28 @@ mod bin_op_tests_in_functions {
     }
 
 
+    #[test]
+    fn binop_arth_vars_only() {
+        for (b, s) in ALL_BIN_OP_KIND_ARTH.iter().zip(BIN_OP_KIND_ARTH_SYMBOLS.iter()) {
+            let stmts = parse_body(&format!("a {} b", s));
+            if let Stmt::Expr(e) = &stmts[0] {
+                if let Expr::BinOp { left, right, op, .. } = &e {
+                    assert_eq!(op, b);
+
+                    assert!(matches!(**left, Expr::Var { .. }));
+                    assert!(matches!(**right, Expr::Var { .. }));
+
+                } else {
+                    panic!("Expected {:?}, instead we got {:?}", b, &e);
+                }
+            } else { panic!("Expected Expr, instead we got {:?}", &stmts[0]) }
+        }
+    }
 
 
 
     #[test]
-    fn binop_arth_vars_only() {
+    fn binop_arth_vars_only_in_var_decl() {
         for t in ALL_TYPES_NO_ARR {
             for (b, s) in ALL_BIN_OP_KIND_ARTH.iter().zip(BIN_OP_KIND_ARTH_SYMBOLS.iter()) {
                 let stmts = parse_body(&format!("own x {} = a {} b", t, s));
@@ -281,6 +374,79 @@ mod bin_op_tests_in_functions {
     // Signed integer literals
     #[test]
     fn binop_arth_vars_and_signed_integer_literals_mixed() {
+        let edge_cases_numbers = [
+            i8::MIN as i128, i8::MAX as i128, 
+            i16::MIN as i128, i16::MAX as i128, 
+            i32::MIN as i128, i32::MAX as i128, 
+            i64::MIN as i128, i64::MAX as i128, 
+            i128::MIN, i128::MAX, 
+        ];
+
+
+        let edge_cases_types = [
+            Type::Int8, Type::Int8,
+            Type::Int16, Type::Int16,
+            Type::Int32, Type::Int32,
+            Type::Int64, Type::Int64,
+            Type::Int128, Type::Int128,
+        ];
+
+
+        for (en, et) in edge_cases_numbers.iter().zip(edge_cases_types.iter()) {    
+            for (b, s) in ALL_BIN_OP_KIND_ARTH.iter().zip(BIN_OP_KIND_ARTH_SYMBOLS.iter()) {
+                let stmts = parse_body(&format!("a {} {}", s, en));
+                if let Stmt::Expr(e) = &stmts[0] {
+                    if let Expr::BinOp { left, right, op, .. } = &e {
+                        assert_eq!(op, b);
+
+                        assert!(matches!(**left, Expr::Var { .. }));
+                        if let Expr::IntLiteral { value, .. } = **right {
+                            assert_eq!(value.get_type(), et.clone());
+
+                            if !value.is_signed() {
+                                panic!("We are in a signed testing function, but value is unsigned: {:?}", **right);
+                            }
+
+                            assert_eq!(value.as_i128(), *en);
+
+                        } else { panic!("Expected IntLiteral, instead got: {:?}", **right) }
+
+                    } else {
+                        panic!("Expected {:?}, instead we got {:?}", b, &e);
+                    }
+                } else { panic!("Expected Expr, instead we got {:?}", &stmts[0]) }
+            }
+
+
+            for (b, s) in ALL_BIN_OP_KIND_ARTH.iter().zip(BIN_OP_KIND_ARTH_SYMBOLS.iter()) {
+                let stmts = parse_body(&format!("{} {} a", en, s));
+                if let Stmt::Expr(e) = &stmts[0] {
+                    if let Expr::BinOp { left, right, op, .. } = &e {
+                        assert_eq!(op, b);
+
+                        if let Expr::IntLiteral { value, .. } = **left {
+                            assert_eq!(value.get_type(), et.clone());
+                            if !value.is_signed() {
+                                panic!("We are in a signed testing function, but value is unsigned: {:?}", **right);
+                            }
+
+                            assert_eq!(value.as_i128(), *en);
+                        } else { panic!("Expected IntLiteral, instead got: {:?}", **left) }
+
+                        assert!(matches!(**right, Expr::Var { .. }));
+
+                    } else {
+                        panic!("Expected {:?}, instead we got {:?}", b, &e);
+                    }
+                } else { panic!("Expected Expr, instead we got {:?}", &stmts[0]) }
+            }
+        }
+    }
+
+
+    // Signed integer literals
+    #[test]
+    fn binop_arth_vars_and_signed_integer_literals_mixed_in_var_decl() {
         let edge_cases_numbers = [
             i8::MIN as i128, i8::MAX as i128, 
             i16::MIN as i128, i16::MAX as i128, 
@@ -447,6 +613,16 @@ mod bin_op_tests_in_functions {
     #[test]
     fn binop_missing_right_operand_errors() {
         let literals_edge_cases = get_all_literals_edge_cases();
+        for l in &literals_edge_cases {
+            for b in BIN_OP_KIND_SYMBOLS {
+                assert_parse_err(&wrap(&format!("{} {}", l, b)));
+            }
+        }
+    }
+
+    #[test]
+    fn binop_missing_right_operand_errors_in_var_decl() {
+        let literals_edge_cases = get_all_literals_edge_cases();
         for t in ALL_TYPES_NO_ARR {
             for l in &literals_edge_cases {
                 for b in BIN_OP_KIND_SYMBOLS {
@@ -456,8 +632,25 @@ mod bin_op_tests_in_functions {
         }
     }
 
+
     #[test]
     fn binop_missing_left_operand_errors() {
+        let literals_edge_cases = get_all_literals_edge_cases();
+        for l in &literals_edge_cases {
+            for b in BIN_OP_KIND_SYMBOLS {
+                // So it's not unary, like -1, which is correct and wouldn't error.
+                if b == "-" {
+                    continue
+                }
+                assert_parse_err(&wrap(&format!("{} {}", b, l)));
+            }
+        }
+    }
+
+
+
+    #[test]
+    fn binop_missing_left_operand_errors_in_var_decl() {
         let literals_edge_cases = get_all_literals_edge_cases();
         for t in ALL_TYPES_NO_ARR {
             for l in &literals_edge_cases {
@@ -474,8 +667,34 @@ mod bin_op_tests_in_functions {
 
     #[test]
     fn binop_nested_via_parens() {
-        // e.g. own x int32 = (1 + 1) + 4
+        // e.g. 
+        // (1 + "hi") + true
         // .. etc
+        //
+        let literals_edge_cases = get_all_literals_edge_cases();
+        for l in &literals_edge_cases {
+            for (b, s) in ALL_BIN_OP_KIND_ARTH.iter().zip(BIN_OP_KIND_ARTH_SYMBOLS.iter()) {
+                let stmts = parse_body(&format!("({} {} {}) {} {}", l, s, l, s, l));
+                if let Stmt::Expr(e) = &stmts[0] {
+                    if let Expr::BinOp { op, left, .. } = &e { 
+                        assert_eq!(op, b);
+                        assert!(matches!(**left, Expr::BinOp { .. }));
+                    } else {
+                        panic!("Expected {:?}, instead we got {:?}", b, &e);
+                    }
+                } else { panic!("Expected Expr, instead we got {:?}", &stmts[0]) }
+            }
+        }
+    }
+
+
+
+    #[test]
+    fn binop_nested_via_parens_in_var_decl() {
+        // e.g. 
+        // own x int32 = (1 + "hi") + true
+        // .. etc
+        //
         let literals_edge_cases = get_all_literals_edge_cases();
         for t in ALL_TYPES_NO_ARR {
             for l in &literals_edge_cases {
