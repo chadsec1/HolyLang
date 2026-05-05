2d9fdfb8d6387fe78e0bf65c83f4034c742b1f0f
ChadSec <freakah1@gmail.com>
2026-05-05 13:04:19 +0300

    tests: Improve parser blackbox unit tests

--- src/parser/blackbox_tests/const_decl_tests.rs
+++ src/parser/blackbox_tests/const_decl_tests.rs
@@ -1,25 +1,70 @@
 use super::*;
 
 #[cfg(test)]
-mod const_decl_tests {
+mod const_decl_in_function_tests {
     use super::*; 
     
     #[test]
     fn const_decl_no_type_errors() {
         let literals_edge_cases = get_all_literals_edge_cases(); 
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
 
-        for l in &literals_edge_cases {
-            assert_parse_err(&wrap(&format!("const x = {}", l)));
+        for l in letters {
+            for lit in &literals_edge_cases {
+                assert_parse_err(&wrap(&format!("const {} = {}", l, lit)));
+            }
+        }
+    }
+
+
+    #[test]
+    fn const_decl_invalid_name_errors() {
+        let int_literals_edge_cases = get_all_ints_literals_edge_cases(); 
+
+        for l in &int_literals_edge_cases {
+            for t in ALL_TYPES_NO_ARR {
+                assert_parse_err(&wrap(&format!("const {} {} = {}", l, t, l)));
+                assert_parse_err(&wrap(&format!("const {} {} = {}", t, l, l)));
+            }
+        }
+    }
+
+
+    #[test]
+    fn const_decl_invalid_type_errors() {
+        let literals_edge_cases = get_all_literals_edge_cases(); 
+
+        let letters: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+
+        for l in letters {
+            for lit in &literals_edge_cases {
+                assert_parse_err(&wrap(&format!("const {} {} = {}", l, lit, lit)));
+                assert_parse_err(&wrap(&format!("const {} {} = {}", l, l, lit)));
+            }
         }
     }
 
     #[test]
-    fn const_decl_in_func() {
+    fn const_decl_no_value_errors() {
+        let literals_edge_cases = get_all_literals_edge_cases(); 
+
+        for t in ALL_TYPES_NO_ARR {
+            assert_parse_err(&wrap(&format!("const x {}", t)));
+        }
+
+        for l in &literals_edge_cases {
+            assert_parse_err(&wrap(&format!("const x {}", l)));
+        }
+    }
+
+    #[test]
+    fn const_decl() {
         let literals_edge_cases = get_all_literals_edge_cases(); 
 
         for l in &literals_edge_cases {
             for t in ALL_TYPES_NO_ARR {
                 let stmts = parse_body(&format!("const x {} = {}", t, l));
+                assert_eq!(stmts.len(), 1);
                 if let Stmt::Const(c) = &stmts[0] {
                     assert_eq!(c.name, "x");
                     assert_eq!(c.type_name, t.clone());
@@ -30,14 +75,42 @@ mod const_decl_tests {
         }
     }
 
+
     #[test]
-    fn const_decl_in_globals() {
+    fn const_decl_in_infinite_loop() {
         let literals_edge_cases = get_all_literals_edge_cases(); 
 
         for l in &literals_edge_cases {
             for t in ALL_TYPES_NO_ARR {
-                let ast = parse(&format!("const x {} = {}", t, l)).unwrap();
-                if let Stmt::Const(c) = &ast.globals[0] {
+                let stmts = parse_body(&format!("infinite {{\nconst x {} = {}\n}}", t, l));
+                assert_eq!(stmts.len(), 1);
+
+                if let Stmt::Infinite(inf) = &stmts[0] {
+                    assert_eq!(inf.branch.len(), 1);
+
+                    if let Stmt::Const(c) = &inf.branch[0] {
+                        assert_eq!(c.name, "x");
+                        assert_eq!(c.type_name, t.clone());
+                    } else { panic!("Expected VarDecl"); }
+                } else { panic!("Expected infinite statement"); }
+            }
+        }
+    }
+
+
+
+
+    // This is semantically not allowed, but it is syntaxally valid.
+    //
+    #[test]
+    fn const_decl_and_assign() {
+        let literals_edge_cases = get_all_literals_edge_cases(); 
+
+        for l in &literals_edge_cases {
+            for t in ALL_TYPES_NO_ARR {
+                let stmts = parse_body(&format!("const x {} = {}\nx = {}", t, l, l));
+                assert_eq!(stmts.len(), 2);
+                if let Stmt::Const(c) = &stmts[0] {
                     assert_eq!(c.name, "x");
                     assert_eq!(c.type_name, t.clone());
                 } else {
@@ -47,17 +120,20 @@ mod const_decl_tests {
         }
     }
 
-    #[test]
-    fn const_decl_no_value_in_func_errors() {
-        for t in ALL_TYPES_NO_ARR {
-            assert_parse_err(&wrap(&format!("const x {}", t)));
-        }
-    }
+}
 
-    #[test]
-    fn const_decl_no_value_in_globals_errors() {
-        for t in ALL_TYPES_NO_ARR {
-            assert_parse_err(&format!("const x {}", t));
-        }
-    }
+
+//
+//
+//
+//
+//
+//
+
+
+#[cfg(test)]
+mod const_decl_in_global_tests {
+    // use super::*; 
+    
+
 }
