a35f5a77c98d4c7b21f013402e7a17403c70fe98
ChadSec <freakah1@gmail.com>
2026-05-08 22:53:54 +0300

    tests: Improve parser_expr unit tests

--- src/parser/parse_expr_tests/array_slicing_tests.rs
+++ src/parser/parse_expr_tests/array_slicing_tests.rs
@@ -21,6 +21,24 @@ mod array_slicing_tests {
     }
 
     #[test]
+    fn test_array_slice_both_bounds_invalid_exprs_errors() {
+        let literals = get_all_literals_edge_cases();
+        
+        for l in literals { 
+            if l.starts_with('-') {
+                continue
+            }
+
+            assert_parse_err(&format!("arr[{} {}:{}]", l, l, l));
+            assert_parse_err(&format!("arr[{}:{} {}]", l, l, l));
+            assert_parse_err(&format!("arr[{} {}:{} {}]", l, l, l, l));
+        }
+    }
+
+
+
+
+    #[test]
     fn test_array_slice_more_than_2_bounds() {
         let literals = get_all_literals_edge_cases();
         
@@ -48,6 +66,21 @@ mod array_slicing_tests {
     }
 
     #[test]
+    fn test_array_slice_start_only_invalid_expr_errors() {
+        let literals = get_all_literals_edge_cases();
+        
+        for l in literals { 
+            if l.starts_with('-') {
+                continue
+            }
+
+            assert_parse_err(&format!("arr[{} {}:]", l, l));
+        }
+    }
+
+
+
+    #[test]
     fn test_array_slice_end_only() {
         let literals = get_all_literals_edge_cases();
         
@@ -62,6 +95,21 @@ mod array_slicing_tests {
             }
         }
     }
+
+    #[test]
+    fn test_array_slice_end_only_invalid_expr_errors() {
+        let literals = get_all_literals_edge_cases();
+        
+        for l in literals { 
+            if l.starts_with('-') {
+                continue
+            }
+
+            assert_parse_err(&format!("arr[:{} {}]", l, l));
+        }
+    }
+
+
 
     #[test]
     fn test_array_slice_both_empty_errors() {
