2ae69d93c37694238f1dbd81f84a049c0b0067eb
ChadSec <freakah1@gmail.com>
2026-05-11 21:38:47 +0300

    tests: Improve existing semantics unit tests and add new tests

--- src/semantic/blackbox_tests/const_tests.rs
+++ src/semantic/blackbox_tests/const_tests.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[cfg(test)]
-mod const_decl_tests {
+mod const_tests {
     use super::*;
 
     #[test]
@@ -9,7 +9,6 @@ mod const_decl_tests {
         let literals = get_all_literals_no_arr();
         
         for (l, t) in literals.iter().zip(ALL_TYPES_NO_ARR.iter()) {
-            //
             let body = vec![const_define("x", t.clone(), l.clone())];
             let func = void_func("foo", vec![], body);
             let mut ast = ast_one(func);
@@ -19,6 +18,46 @@ mod const_decl_tests {
                 assert_eq!(c.type_name, t.clone());
                 assert_eq!(c.value, l.clone());
             } else { panic!("expected Const, got {:?}", ast); }
+        }
+    }
+
+
+    #[test]
+    fn type_mismatch_literals_errors() {
+        let literals_no_ints = get_all_literals_no_arr_no_ints();
+
+        for t in ALL_INT_TYPES_NO_ARR {
+            for l in &literals_no_ints {
+                let body = vec![const_define("x", t.clone(), l.clone())];
+                let func = void_func("foo", vec![], body);
+                let mut ast = ast_one(func);
+                let result = check_semantics(&mut ast);
+
+                assert!(result.is_err());        
+                assert!(result.unwrap_err().to_string().contains("Type mismatch assigning to"));
+            }
+        }
+    }
+
+    #[test]
+    fn dynamic_arrays_errors() {
+        let literals = get_all_literals_no_arr();
+        
+        for (l, t) in literals.iter().zip(ALL_TYPES_NO_ARR.iter()) {
+            for i in 0..=100 {
+                let elements = vec![l.clone(); i];
+
+                let arr_lit = Expr::ArrayLiteral { elements: elements, span: span() };
+
+                let body = vec![const_define("x", Type::Array(Box::new(t.clone())), arr_lit.clone())];
+             
+                let func = void_func("foo", vec![], body);
+                let mut ast = ast_one(func);
+                let result = check_semantics(&mut ast);
+
+                assert!(result.is_err());        
+                assert!(result.unwrap_err().to_string().contains("Dynamic arrays cannot be evaluated at compile time"));
+            }
         }
     }
 
