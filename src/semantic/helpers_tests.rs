53fa733632e0b241354381eb7af073c62122118d
ChadSec <freakah1@gmail.com>
2026-05-13 18:32:30 +0300

    tests: Remove test functions already covered in AST tests

--- src/semantic/helpers_tests.rs
+++ src/semantic/helpers_tests.rs
@@ -1,155 +1,19 @@
 use super::*;
 
-
-use crate::ast::{
-    IntLiteralValue
-};
-
 use crate::tests_consts::{
-    ALL_TYPES_NO_ARR,
     ALL_TYPES_NO_INTS_NO_ARR,
     ALL_INT_TYPES_NO_ARR
 
 };
 
 use crate::semantic::helpers::{
-    assign_default_value_for_type,
-    get_bigger_type_of_two_integers,
+    get_bigger_type_of_two_integers
 };
 
 
 #[cfg(test)]
 mod helpers_tests {
     use super::*;
-
-    fn dummy_span() -> Span {
-        Span { line: 1, column: 1 }
-    }
-
-    // assign_default_value_for_type
-    //
-    #[test]
-    fn default_value_signed_integers_are_zero() {
-        let cases: &[(Type, IntLiteralValue)] = &[
-            (Type::Int8,   IntLiteralValue::Int8(0)),
-            (Type::Int16,  IntLiteralValue::Int16(0)),
-            (Type::Int32,  IntLiteralValue::Int32(0)),
-            (Type::Int64,  IntLiteralValue::Int64(0)),
-            (Type::Int128, IntLiteralValue::Int128(0)),
-        ];
-        for (ty, expected_lit) in cases {
-            let mut expr: Option<Expr> = None;
-            assign_default_value_for_type(&mut expr, ty, dummy_span()).unwrap();
-            match expr.unwrap() {
-                Expr::IntLiteral { value, .. } => {
-                    assert_eq!(value, *expected_lit,
-                        "Default for {:?} should be {:?}", ty, expected_lit);
-                }
-                other => panic!("Expected IntLiteral for {:?}, got {:?}", ty, other),
-            }
-        }
-    }
-
-    #[test]
-    fn default_value_unsigned_integers_are_zero() {
-        let cases: &[(Type, IntLiteralValue)] = &[
-            (Type::Byte,    IntLiteralValue::Byte(0)),
-            (Type::Uint16,  IntLiteralValue::Uint16(0)),
-            (Type::Uint32,  IntLiteralValue::Uint32(0)),
-            (Type::Uint64,  IntLiteralValue::Uint64(0)),
-            (Type::Uint128, IntLiteralValue::Uint128(0)),
-            (Type::Usize,   IntLiteralValue::Usize(0)),
-        ];
-        for (ty, expected_lit) in cases {
-            let mut expr: Option<Expr> = None;
-            assign_default_value_for_type(&mut expr, ty, dummy_span()).unwrap();
-            match expr.unwrap() {
-                Expr::IntLiteral { value, .. } => {
-                    assert_eq!(value, *expected_lit,
-                        "Default for {:?} should be {:?}", ty, expected_lit);
-                }
-                other => panic!("Expected IntLiteral for {:?}, got {:?}", ty, other),
-            }
-        }
-    }
-
-    #[test]
-    fn default_value_float64_is_zero() {
-        let mut expr: Option<Expr> = None;
-        assign_default_value_for_type(&mut expr, &Type::Float64, dummy_span()).unwrap();
-        match expr.unwrap() {
-            Expr::Float64Literal { value, .. } => {
-                assert_eq!(value, 0.0f64);
-            }
-            other => panic!("Expected Float64 literal, got {:?}", other),
-        }
-    }
-
-    #[test]
-    fn default_value_bool_is_false() {
-        let mut expr: Option<Expr> = None;
-        assign_default_value_for_type(&mut expr, &Type::Bool, dummy_span()).unwrap();
-        match expr.unwrap() {
-            Expr::BoolLiteral { value: false, .. } => {}
-            other => panic!("Expected BoolLiteral false, got {:?}", other),
-        }
-    }
-
-    #[test]
-    fn default_value_string_is_empty() {
-        let mut expr: Option<Expr> = None;
-        assign_default_value_for_type(&mut expr, &Type::String, dummy_span()).unwrap();
-        match expr.unwrap() {
-            Expr::StringLiteral { value, .. } => {
-                assert_eq!(value, "", "Default string must be empty");
-            }
-            other => panic!("Expected StringLiteral, got {:?}", other),
-        }
-    }
-
-    #[test]
-    fn default_value_array_is_empty_with_correct_inner_type() {
-        for t in ALL_TYPES_NO_ARR {
-            let mut expr: Option<Expr> = None;
-            let arr_t = Type::Array(Box::new(t.clone()));
-            assign_default_value_for_type(&mut expr, &arr_t, dummy_span()).unwrap();
-            match expr.unwrap() {
-                Expr::ArrayLiteral { elements, .. } => {
-                    assert!(elements.is_empty(), "Default array must be empty");
-                }
-                other => panic!("Expected ArrayLiteral, got {:?}", other),
-            }
-            
-        }
-    }
-
-    // Nested array: inner type must also be preserved correctly
-    #[test]
-    fn default_value_nested_array_preserves_inner_type() {
-        for t in ALL_TYPES_NO_ARR {
-            let mut expr: Option<Expr> = None;
-            let arr_t = Type::Array(Box::new(Type::Array(Box::new(t.clone()))));
-            assign_default_value_for_type(&mut expr, &arr_t, dummy_span()).unwrap();
-            match expr.unwrap() {
-                Expr::ArrayLiteral { elements, .. } => {
-                    assert!(elements.is_empty());
-                }
-                other => panic!("Expected nested ArrayLiteral, got {:?}", other),
-            }
-        }
-    }
-
-    // Calling on an existing Some(...) value, should trigger a panic guard
-    #[should_panic(expected = "Compiler bug")]
-    #[test]
-    fn default_value_overwrites_existing_some() {
-        let mut expr: Option<Expr> = Some(Expr::IntLiteral {
-            value: IntLiteralValue::Int32(999),
-            span: dummy_span(),
-        });
-        assign_default_value_for_type(&mut expr, &Type::Int32, dummy_span()).unwrap();
-    }
-
 
     // get_bigger_type_of_two_integers
 
