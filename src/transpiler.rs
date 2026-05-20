e4ee1f9b8c486196e2f8651d996890c6b6cae99d
ChadSec <freakah1@gmail.com>
2026-05-21 02:05:45 +0300

    feat: Add more types and expressions support in the transpiler

--- src/transpiler.rs
+++ src/transpiler.rs
@@ -1,5 +1,5 @@
 use crate::ast::{
-    AST, GlobalStmt, Type, Expr
+    AST, Function, Stmt, GlobalStmt, Type, Expr
 };
 
 /// Takes a reference to a Abstract Syntax Tree, and returns equvilent code in Rust as a string
@@ -7,15 +7,59 @@ use crate::ast::{
 pub fn transpile(ast: &AST) -> String {
     let mut rcode: String = String::new();
 
-
     for global_stmt in &ast.globals {
         let global_stmt_rcode = transpile_global_stmt(global_stmt);
 
         rcode = format!("{}{}\n", rcode, global_stmt_rcode);
     }
 
+    for func in &ast.functions {
+        let func_rcode = transpile_function(func);
+
+        rcode = format!("{}{}\n", rcode, func_rcode);
+    }
 
     return rcode
+}
+
+
+
+/// Transpiles a function and its inner statements into equvilent Rust code
+///
+fn transpile_function(func: &Function) -> String {
+    let mut func_rcode: String = "fn".to_string();
+
+    func_rcode = format!("{} {}() {{\n", func_rcode, func.name.clone());
+    
+    for stmt in &func.body {
+        let stmt_rcode = transpile_stmt(stmt);
+        func_rcode = format!("{} {}\n", func_rcode, stmt_rcode);
+    }
+    func_rcode = format!("{}\n}}", func_rcode);
+    return func_rcode
+}
+
+
+
+/// Transpiles a statement into equivlent Rust code
+///
+fn transpile_stmt(stmt: &Stmt) -> String {
+    match stmt {
+        Stmt::VarDecl(var) => {
+            let mut var_stmt_rcode: String = "let".to_string();
+
+            let var_type = holy_type_to_rust_type_str(&var.type_name);
+            let var_value = expr_to_rust_expr(&var.value);
+
+            var_stmt_rcode = format!("{} {}", var_stmt_rcode, var.name.clone());
+            var_stmt_rcode = format!("{}: {} =", var_stmt_rcode, var_type);
+            var_stmt_rcode = format!("{} {};", var_stmt_rcode, var_value);
+
+            return var_stmt_rcode;
+        },
+
+        _ => todo!()
+    }
 }
 
 
@@ -27,7 +71,7 @@ fn transpile_global_stmt(global_stmt: &GlobalStmt) -> String {
             let mut const_stmt_rcode: String = "const".to_string();
 
             let const_type = holy_type_to_rust_type_str(&cons.type_name);
-            let const_value = literal_expr_to_rust_literal_str(&cons.value);
+            let const_value = expr_to_rust_expr(&cons.value);
 
             const_stmt_rcode = format!("{} {}", const_stmt_rcode, cons.name.clone());
             const_stmt_rcode = format!("{}: {} =", const_stmt_rcode, const_type);
@@ -41,10 +85,9 @@ fn transpile_global_stmt(global_stmt: &GlobalStmt) -> String {
 }
 
 
-/// Turns a literal expression e.g. IntLiteral, BoolLiteral, Float64Literal, StringLiteral, etc,
-/// into equvilent Rust value
+/// Turns a HolyLang expression, into equvilent Rust expression
 ///
-fn literal_expr_to_rust_literal_str(expr: &Expr) -> String {
+fn expr_to_rust_expr(expr: &Expr) -> String {
     match expr {
         Expr::IntLiteral { value, .. } => {
             let value_ty = holy_type_to_rust_type_str(&value.get_type());
@@ -58,7 +101,14 @@ fn literal_expr_to_rust_literal_str(expr: &Expr) -> String {
                 return format!("{}{}", value_raw, value_ty);
             }
         },
+        
+        Expr::Float64Literal { value, .. } => {
+            return format!("{}f64", value);
+        }
 
+        Expr::BoolLiteral { value, .. } => {
+            return value.to_string();
+        }
         _ => todo!()
     }
 
@@ -69,7 +119,21 @@ fn literal_expr_to_rust_literal_str(expr: &Expr) -> String {
 ///
 fn holy_type_to_rust_type_str(holy_type: &Type) -> String {
     match holy_type {
+        Type::Int8 => "i8".to_string(),
+        Type::Int16 => "i16".to_string(),
         Type::Int32 => "i32".to_string(),
+        Type::Int64 => "i64".to_string(),
+        Type::Int128 => "i128".to_string(),
+
+        Type::Byte => "u8".to_string(),
+        Type::Uint16 => "u16".to_string(),
+        Type::Uint32 => "u32".to_string(),
+        Type::Uint64 => "u64".to_string(),
+        Type::Uint128 => "u128".to_string(),
+        Type::Usize => "usize".to_string(),
+        
+        Type::Float64 => "f64".to_string(),
+        Type::Bool => "bool".to_string(),
 
         _ => todo!()
     }
