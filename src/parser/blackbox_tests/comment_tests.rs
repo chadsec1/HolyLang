677fa59c4938364ac07ed6f5fb574cf38bbdb115
ChadSec <freakah1@gmail.com>
2026-05-24 11:35:03 +0300

    tests: Add more unit tests for comments parsing in the parser

--- src/parser/blackbox_tests/comment_tests.rs
+++ src/parser/blackbox_tests/comment_tests.rs
@@ -5,7 +5,40 @@ mod comment_tests {
     use super::*;
 
     #[test]
-    fn inline_comment_stripped() {
+    fn const_in_globals_inline_comment_stripped() {
+        let literals_edge_cases = get_all_literals_edge_cases();
+
+        for t in ALL_TYPES_NO_ARR {
+            for l in &literals_edge_cases {
+                // Statement followed by inline comment should still parse cleanly
+                let ast = parse(&format!("const x {} = {} # this is {} a comment", t, l, t)).unwrap();
+                assert_eq!(ast.functions.len(), 0);
+                assert_eq!(ast.globals.len(), 1);
+
+                if let GlobalStmt::Const(c) = &ast.globals[0] {
+                    assert_eq!(c.name, "x");
+                    assert_eq!(c.type_name, t.clone());
+                } else { panic!("Expected Const"); }
+            }
+        }
+    }
+
+    #[test]
+    fn const_in_func_inline_comment_stripped() {
+        let literals_edge_cases = get_all_literals_edge_cases();
+
+        for t in ALL_TYPES_NO_ARR {
+            for l in &literals_edge_cases {
+                // Statement followed by inline comment should still parse cleanly
+                let stmts = parse_body(&format!("const x {} = {} # this is {} a comment", t, l, t));
+                assert_eq!(stmts.len(), 1);
+                assert!(matches!(stmts[0], Stmt::Const(_)));
+            }
+        }
+    }
+
+    #[test]
+    fn var_decl_inline_comment_stripped() {
         let literals_edge_cases = get_all_literals_edge_cases();
 
         for t in ALL_TYPES_NO_ARR {
