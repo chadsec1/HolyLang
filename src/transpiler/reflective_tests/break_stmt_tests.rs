d4e93ec45107a290877df9851a6713e40a7b9784
ChadSec <freakah1@gmail.com>
2026-05-29 18:26:42 +0300

    tests: Add new unit tests for break statements in the transpiler layer

--- src/transpiler/reflective_tests/break_stmt_tests.rs
+++ src/transpiler/reflective_tests/break_stmt_tests.rs
@@ -206,4 +206,153 @@ mod break_stmt_in_void_func_tests {
         }
     }
 
+    #[test]
+    fn break_stmt_in_if_stmt_literal_condition() {
+        let literals = get_all_literals();
+
+        for l in literals {
+            let body = vec![
+                Stmt::If(IfStmt{
+                        condition: l.clone(),
+                        if_branch: vec![
+                            Stmt::Break(BreakStmt { span: span() })
+                        ],
+                        elif_branches: vec![],
+                        else_branch: None,
+                        span: span(),
+                    }),
+            ];
+
+            let func = void_func("foo", vec![], body);
+            let ast = &ast_one(func);
+
+            let internals = import_internals();
+            let rcode = transpile(ast);
+            assert!(rcode.starts_with(&internals));
+            let rcode = rcode[internals.len()..].replace('\n', "");
+
+            let l_str = holy_expr_to_rust_expr(&l);
+
+            assert_eq!(rcode, format!("fn foo() {{ if {} {{break;}}}}", l_str));
+        }
+    }
+
+    #[test]
+    fn break_stmt_in_if_stmt_binop_condition() {
+        let literals = get_all_literals();
+
+        for l in literals {
+            for b in ALL_BIN_OP_KIND {
+                let bin = Expr::BinOp {
+                    left: Box::new(l.clone()),
+                    op: b,
+                    right: Box::new(l.clone()),
+                    span: span(),
+                };
+
+
+                let body = vec![
+                    Stmt::If(IfStmt{
+                            condition: bin.clone(),
+                            if_branch: vec![
+                                Stmt::Break(BreakStmt { span: span() })
+                            ],
+                            elif_branches: vec![],
+                            else_branch: None,
+                            span: span(),
+                        }),
+                ];
+
+                let func = void_func("foo", vec![], body);
+                let ast = &ast_one(func);
+
+                let internals = import_internals();
+                let rcode = transpile(ast);
+                assert!(rcode.starts_with(&internals));
+                let rcode = rcode[internals.len()..].replace('\n', "");
+
+                let bin_str = holy_expr_to_rust_expr(&bin);
+
+                assert_eq!(rcode, format!("fn foo() {{ if {} {{break;}}}}", bin_str));
+            }
+        }
+    }
+
+
+    #[test]
+    fn break_stmt_in_if_else_stmt_literal_condition() {
+        let literals = get_all_literals();
+
+        for l in literals {
+            let body = vec![
+                Stmt::If(IfStmt{
+                        condition: l.clone(),
+                        if_branch: vec![
+                            Stmt::Break(BreakStmt { span: span() })
+                        ],
+                        elif_branches: vec![],
+                        else_branch: Some(vec![
+                            Stmt::Break(BreakStmt { span: span() })
+                        ]),
+                        span: span(),
+                    }),
+            ];
+
+            let func = void_func("foo", vec![], body);
+            let ast = &ast_one(func);
+
+            let internals = import_internals();
+            let rcode = transpile(ast);
+            assert!(rcode.starts_with(&internals));
+            let rcode = rcode[internals.len()..].replace('\n', "");
+
+            let l_str = holy_expr_to_rust_expr(&l);
+
+            assert_eq!(rcode, format!("fn foo() {{ if {} {{break;}} else {{break;}}}}", l_str));
+        }
+    }
+
+    #[test]
+    fn break_stmt_in_if_else_stmt_binop_condition() {
+        let literals = get_all_literals();
+
+        for l in literals {
+            for b in ALL_BIN_OP_KIND {
+                let bin = Expr::BinOp {
+                    left: Box::new(l.clone()),
+                    op: b,
+                    right: Box::new(l.clone()),
+                    span: span(),
+                };
+
+
+                let body = vec![
+                    Stmt::If(IfStmt{
+                            condition: bin.clone(),
+                            if_branch: vec![
+                                Stmt::Break(BreakStmt { span: span() })
+                            ],
+                            elif_branches: vec![],
+                            else_branch: Some(vec![
+                                Stmt::Break(BreakStmt { span: span() })
+                            ]),
+                            span: span(),
+                        }),
+                ];
+
+                let func = void_func("foo", vec![], body);
+                let ast = &ast_one(func);
+
+                let internals = import_internals();
+                let rcode = transpile(ast);
+                assert!(rcode.starts_with(&internals));
+                let rcode = rcode[internals.len()..].replace('\n', "");
+
+                let bin_str = holy_expr_to_rust_expr(&bin);
+
+                assert_eq!(rcode, format!("fn foo() {{ if {} {{break;}} else {{break;}}}}", bin_str));
+            }
+        }
+    }
+
 }
