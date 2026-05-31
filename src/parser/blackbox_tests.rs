9a5bbdf83660d5943a8d9adf113898df0cc461d8
ChadSec <freakah1@gmail.com>
2026-05-31 19:00:38 +0300

    fix: Fix nested array literals and array access misparsing bugs

--- src/parser/blackbox_tests.rs
+++ src/parser/blackbox_tests.rs
@@ -90,7 +90,7 @@ fn span() -> Span {
 // all literals (ints, floats, array, strings literals of ints, floats, strings, other arrays, and variable names, and array access and slicing.
 // Some of these literals are illegal semantically, but syntaxally, should be valid.
 //
-fn get_all_literals_edge_cases() -> [String; 126] {
+fn get_all_literals_edge_cases() -> [String; 139] {
     return [
         i8::MIN.to_string(), i8::MAX.to_string(),
         i16::MIN.to_string(), i16::MAX.to_string(),
@@ -109,7 +109,13 @@ fn get_all_literals_edge_cases() -> [String; 126] {
 
         "false".to_string(), "true".to_string(),
         "\"\"".to_string(), "\"h\"".to_string(), "\"hi\"".to_string(),
-        "\"hi, lmao\"".to_string(),
+        "\"hi, lmao\"".to_string(),  "\"hi ] lmao\"".to_string(), "\"hi [ lmao\"".to_string(), 
+        "\"foo]\"".to_string(), "\"foo[\"".to_string(),
+        "\"]foo\"".to_string(), "\"[foo\"".to_string(),
+        "\"]foo]\"".to_string(), "\"[foo[\"".to_string(),
+        "\"[foo]\"".to_string(),
+        "\"foo\\\"]\"".to_string(), "\"foo\\\"[\"".to_string(),
+        "\"\\\"]foo\"".to_string(), "\"\\\"[foo[\"".to_string(),
         "i".to_string(), "arr".to_string(), "x".to_string(), "y".to_string(), "xyz".to_string(),
         
         format!("arr[{}]", i8::MIN.to_string()), format!("arr[{}]", i8::MAX.to_string()), 
