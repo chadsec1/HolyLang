33b0ce6a72a6ee4440fdf32428658c4bd95a7049
ChadSec <freakah1@gmail.com>
2026-06-01 20:38:21 +0300

    fix: Require main function

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,10 @@ pub fn compile_holylang_src(source: &str, compile_info: CompileInfo) -> String {
 
     println!("Pure AST: {ast:#?}\n\n\n");
 
+    if !ast.functions.iter().any(|f| f.name == "main") {
+        panic!("Missing `main` function");
+    }
+
     // Run semantic checks, enforce language rules, modify AST to remove inferred and replace with explicit types, etc.
     semantic::check_semantics(&mut ast).expect("Semantic errors");
     println!("Checked AST: {ast:#?}\n\n\n");
